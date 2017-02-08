aad20516836792a032e62392736a2417e5b1fa1b
RahellBarnawi <raheel_barnawi@hotmail.com>
2017-02-07 20:20:39 -0500

    changed the code of the first map

--- Assignment1/src/WhoToFollow.java
+++ Assignment1/src/WhoToFollow.java
@@ -2,19 +2,19 @@
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
 import java.io.IOException;
+import org.apache.hadoop.fs.Path;
 import java.util.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.jobcontrol.JobControl;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
@@ -23,138 +23,115 @@ public class WhoToFollow extends Configured implements Tool
 
 {
 
-	public static void main(String[] args) throws Exception
+	private static final String OUTPUT_PATH = "intermediate_output";
 
-	{
-		if (args.length != 2)
-////
-		{
-			System.out.println("Usage: FriendRecommendation <input dir> <output dir> \n");
-			System.exit(-1);
-		}
+	 @Override
+	 public int run(String[] args) throws Exception {
+	  /*
+	   * Job 1
+	   */
+	  Configuration conf = getConf();
+	 // FileSystem fs = FileSystem.get(conf);
+	  Job job = new Job(conf, "Job1");
+	  job.setJarByClass(WhoToFollow.class);
 
-		System.out.println(Arrays.toString(args));
-		int res = ToolRunner.run(new Configuration(), new WhoToFollow(), args);
-		System.exit(res);
-	}
+	  job.setMapperClass(Mapper.class);
+	  job.setReducerClass(Reducer.class);
 
-	public int run(String[] args) throws Exception {
-		System.out.println(Arrays.toString(args));
-		@SuppressWarnings("deprecation")
-		String intermediateFileDir = "tmp1";
-		String intermediateFileDirFile = intermediateFileDir + "/part-r-00000";
-		JobControl control = new JobControl("ChainMapReduce");
-		ControlledJob step1 = new ControlledJob(jobListFriends(args[0], intermediateFileDir), null);
-		ControlledJob step2 = new ControlledJob(jobRecommendFriends(intermediateFileDirFile, args[1]),Arrays.asList(step1));
-		control.addJob(step1);
-		control.addJob(step2);
-		Thread workFlowThread = new Thread(control, "workflowthread");
-		workFlowThread.setDaemon(true);
-		workFlowThread.start();
-		return 0;
+	  job.setOutputKeyClass(IntWritable.class);
+	  job.setOutputValueClass(IntWritable.class);
+	  //job.setMapOutputKeyClass(IntWritable.class);
+	 // job.setMapOutputValueClass(IntWritable.class);
 
-	}
+	 
+	  job.setOutputFormatClass(TextOutputFormat.class);
+	 job.setInputFormatClass(KeyValueTextInputFormat.class);
 
-	/*
-	 * The code above is the driver for the chained MapReduce jobs. The file
-	 * output path for MapReduce job 1 must match exactly with the file input
-	 * path for MapReduce job 2. Another critical issue is that we need to
-	 * specify the dependency in "ControlledJob step2" so that MapReduce job 2
-	 * will only start to run after the first MapReduce job completes.
-	 */
+	 FileInputFormat.addInputPath(job, new Path(args[0]));
+	  TextOutputFormat.setOutputPath(job, new Path(OUTPUT_PATH));
 
-	/* The code above setup all the configuration for MapReduce job 1. */
+	  job.waitForCompletion(true);
 
-	private Job jobListFriends(String inputPath, String outputPath)
-			throws IOException, InterruptedException, ClassNotFoundException {
+	  /*
+	   * Job 2
+	   */
+	  
+	  Job job2 = new Job(conf, "Job 2");
+	  job2.setJarByClass(WhoToFollow.class);
 
-		Job job = new Job();
+	  job2.setMapperClass(MapRecommendation.class);
+	  job2.setReducerClass(ReduceRecommendation.class);
 
-		job.setJarByClass(WhoToFollow.class);
-		job.setOutputKeyClass(IntWritable.class);
-		job.setOutputValueClass(IntWritable.class);
-		job.setMapperClass(Mapper.class);
-		job.setReducerClass(Reducer.class);
-		job.setInputFormatClass(KeyValueTextInputFormat.class); // Need to																												
-		job.setOutputFormatClass(TextOutputFormat.class);// ???
-		FileInputFormat.addInputPath(job, new Path(inputPath));
-		FileOutputFormat.setOutputPath(job, new Path(outputPath));
-		job.waitForCompletion(true);
-		return job;
-	}
+	  //reducer output(k,v) classes 
+	  job2.setOutputKeyClass(IntWritable.class);
+	  job2.setOutputValueClass(IntWritable.class);
+	  
+	  //job2.setMapOutputKeyClass(IntWritable.class);
+	  //job2.setMapOutputValueClass(IntWritable.class);
 
-	/* The code above setup all the configuration for MapReduce job 2. */
 
-	private Job jobRecommendFriends(String inputPath, String outputPath)
-			throws IOException, InterruptedException, ClassNotFoundException {
+	 job2.setInputFormatClass(SequenceFileInputFormat.class);
+	 job2.setOutputFormatClass(TextOutputFormat.class);
 
-		@SuppressWarnings("deprecation")
-		Job job1 = new Job();
-		job1.setJarByClass(WhoToFollow.class);
-		job1.setOutputKeyClass(IntWritable.class);
-		job1.setOutputValueClass(IntWritable.class);
-		job1.setMapperClass(MapRecommendation.class);
-		job1.setReducerClass(ReduceRecommendation.class);
-		job1.setOutputFormatClass(TextOutputFormat.class);
-		job1.setInputFormatClass(KeyValueTextInputFormat.class);
-		FileInputFormat.addInputPath(job1, new Path(inputPath));
-		FileOutputFormat.setOutputPath(job1, new Path(outputPath));
-		job1.waitForCompletion(true);
-		return job1;
-	}
+	  FileInputFormat.addInputPath(job2, new Path(OUTPUT_PATH));
+	  TextOutputFormat.setOutputPath(job2, new Path(args[1]));
 
-	/*
-	 * The code above is the Mapper for the first MapReduce job.
-	 * 
-	 * We need add a mark "zero" here if the pair <userID1, userID2>
-	 * 
-	 * are found to be friends already which will be discared in the Reducer
-	 * stage.
-	 */
+	  return job2.waitForCompletion(true) ? 0 : 1;
+	 }
 
-	public static class Map extends Mapper<Object, Text, IntWritable, IntWritable> {
+	 /**
+	  * Method Name: main Return type: none Purpose:Read the arguments from
+	  * command line and run the Job till completion
+	  * 
+	  */
+	 public static void main(String[] args) throws Exception {
+	  // TODO Auto-generated method stub
+	  if (args.length != 2) {
+	   System.err.println("Enter valid number of arguments <Inputdirectory>  <Outputlocation>");
+	   System.exit(0);
+	  }
+	  System.out.println("Hi there ");
+	  ToolRunner.run(new Configuration(), new WhoToFollow(), args);
+	 }
+
+	
+
+	public static class Map extends Mapper<Text, Text, IntWritable, IntWritable> {
 		public final Log log = LogFactory.getLog(Mapper.class);
-		@Override
-
-		public void map(Object key, Text value, Context context) throws IOException, InterruptedException
+	
+		
+		public void map(Text key, Text value, Context context) throws IOException, InterruptedException
 		{
-			log.info("in side the map ");
+			
+			
 			StringTokenizer itr = new StringTokenizer(value.toString());
 			IntWritable friend1 = new IntWritable();
-			// IntWritable friend2 = new IntWritable();
 			IntWritable followed = new IntWritable();
 			IntWritable user = new IntWritable();
 			ArrayList<Integer> frinedList = new ArrayList<>();
 			user.set(Integer.parseInt(itr.nextToken()));
 			while (itr.hasMoreTokens())
 			{
+				// convert all the string tokens to integer
 				frinedList.add(Integer.parseInt(itr.nextToken()));
 			}
 
 			for (Integer friend : frinedList)
 			{
-			friend1.set(friend);
-				context.write(friend1, user);
-				followed.set(-friend);
+			    friend1.set(friend);
+				context.write(friend1, user);// emit friend as a key and user as a value 
+				followed.set(-friend);// use the - trick to keep track on people followed by user
 				context.write(user, followed);
 			}
 		}
 	}
 
-	/*
-	 * The code is the Reducer for the first MapReduce job.
-	 * 
-	 * We need delete the pair <userID1, userID2>
-	 * 
-	 * which are found to be friends already and marked with "Zero"
-	 * 
-	 * in the value of the input in the Mapper stage.
-	 */
+	
 
 	public static class Reduce extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable>
 
 	{
-
+		
 		public void reduce(IntWritable key, Iterable<IntWritable> values, Context context)
 				throws IOException, InterruptedException {
 			ArrayList<Integer> existingFriends = new ArrayList();
@@ -168,106 +145,30 @@ public class WhoToFollow extends Configured implements Tool
 				// counter++;
 			}
 
-			/*
-			 * IntWritable[] temp1 = new IntWritable[existingFriends.size()];
-			 * 
-			 * for(int i=0 ; i<temp1.length;i++)
-			 * 
-			 * {
-			 * 
-			 * temp.set(existingFriends.get(i));
-			 * 
-			 * temp1[i] = temp;
-			 * 
-			 * }
-			 */
+			
 			context.write(key, temp);
 		}
 	}
 
 	/* The Mapper for the second MapReduce job */
 
-	public static class MapRecommendation extends Mapper<Text, Text, IntWritable, IntWritable>
+	public static class MapRecommendation extends Mapper<IntWritable, IntWritable, IntWritable, IntWritable>
 
 	{
 
-		public void map(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException
+	
+		public void map(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
 
 		{
 
-			// ArrayList<Integer> existingFriends = new ArrayList();
-
-			// ArrayList<Integer> recommendedUsers = new ArrayList<>();
+		
 
 			IntWritable friend1 = new IntWritable(1);
 			IntWritable friend2 = new IntWritable(2);
 			Text word = new Text();
 			word.set("Raheel");
 
-			/*
-			 * IntWritable temp = new IntWritable();
-			 * 
-			 * IntWritable count = new IntWritable();
-			 * 
-			 * IntWritable length = new IntWritable();
-			 */
-
-			/*
-			 * while (values.iterator().hasNext())
-			 * 
-			 * {
-			 * 
-			 * String value = values.iterator().next().get();
-			 * 
-			 * if (value > 0)
-			 * 
-			 * {
-			 * 
-			 * //logR.info("enter if " );
-			 * 
-			 * recommendedUsers.add(value);
-			 * 
-			 * } else
-			 * 
-			 * {
-			 * 
-			 * //logR.info("enter else " );
-			 * 
-			 * existingFriends.add(value);
-			 * 
-			 * }
-			 * 
-			 * }
-			 */
-
-			/*
-			 * 
-			 * for (int i = 0; i < recommendedUsers.size(); i++)
-			 * 
-			 * {
-			 * 
-			 * friend1.set(recommendedUsers.get(i));
-			 * 
-			 * for (int j = 0; j < recommendedUsers.size(); j++)
-			 * 
-			 * {
-			 * 
-			 * 
-			 * 
-			 * if (recommendedUsers.get(i)!= recommendedUsers.get(j))
-			 * 
-			 * {
-			 * 
-			 * friend2.set(recommendedUsers.get(j));
-			 * 
-			 * context.write(friend1, friend2);
-			 * 
-			 * }
-			 * 
-			 * }
-			 * 
-			 * }
-			 */
+			
 
 			context.write(friend1, friend2);
 
