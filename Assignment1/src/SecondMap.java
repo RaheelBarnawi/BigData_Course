31fe000affe24fdd98e9e0d5c8dabd580a7893fd
RahellBarnawi <raheel_barnawi@hotmail.com>
2017-02-08 21:57:00 -0500

    cleaned up the code and made it more readable

--- Assignment1/src/SecondMap.java
+++ Assignment1/src/SecondMap.java
@@ -1,70 +1,57 @@
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
-
-//import org.apache.commons.logging.Log;
-//import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Mapper.Context;
-
+//import org.apache.hadoop.mapreduce.Mapper.Context;
 
 public  class SecondMap extends Mapper<Object, Text, IntWritable, IntWritable>
 
 	{
 		//public final Log logR = LogFactory.getLog(ReduceRecommendation.class);
 
-		public void map(Object key, Text values, Context context) //VHS - modified
+		public void map(Object key, Text values, Context context) 
 				throws IOException, InterruptedException
 
 		{
-			//logR.info("Seecond Map");
 			StringTokenizer itr = new StringTokenizer(values.toString());
 			ArrayList<Integer> existingFriends = new ArrayList();
 			ArrayList<Integer> recommendedUsers = new ArrayList<>();
 			IntWritable friend1 = new IntWritable();
 			IntWritable friend2 = new IntWritable();
-			IntWritable temp = new IntWritable();
+			IntWritable existingFriend = new IntWritable();
 			ArrayList<Integer> frinedList = new ArrayList<>();
-			ArrayList<Integer> test_input = new ArrayList<Integer>() {
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				public String toString() {
-					return super.toString();
-				}
-			};
-			
-			Integer userKey = new Integer(itr.nextToken());//VHS - modified
-
-			while (itr.hasMoreTokens()) {
-				
-				frinedList.add(Integer.parseInt(itr.nextToken()));
+			Integer userKey = new Integer(itr.nextToken());
+			while (itr.hasMoreTokens()) 
+			{frinedList.add(Integer.parseInt(itr.nextToken()));// converts all tokens to type Integer
 			}
-			for (Integer friend : frinedList){
-				test_input.add(friend);
-				// logR.info("print value"+ value );
+			
+			for (Integer friend : frinedList)
+			{
 				if (friend > 0)
 					recommendedUsers.add(friend);
 				else
 					existingFriends.add(friend);
 			}
 
-			//logR.info(" second map _values" + test_input.toString());
-
-			for (int i = 0; i < recommendedUsers.size(); i++) {
+			
+			for (int i = 0; i < recommendedUsers.size(); i++)
+			{
 				friend1.set(recommendedUsers.get(i));
-				for (int j = 0; j < recommendedUsers.size(); j++) {
-					if (recommendedUsers.get(i) != recommendedUsers.get(j)) {
+				for (int j = 0; j < recommendedUsers.size(); j++) 
+				{
+					if (recommendedUsers.get(i) != recommendedUsers.get(j)) 
+					{
 						friend2.set(recommendedUsers.get(j));
-						context.write(friend1, friend2);
+						context.write(friend1, friend2);// emit (a,b)(b,a) 
 					}
 				}
 			}
-			for (int i = 0; i < existingFriends.size(); i++) {
-				temp.set(existingFriends.get(i));
-				context.write(new IntWritable(userKey), temp);//VHS - modified
+			for (int i = 0; i < existingFriends.size(); i++) 
+			{
+				existingFriend.set(existingFriends.get(i));
+				context.write(new IntWritable(userKey), existingFriend);// emit existingFriend 
 			}
 		}
 
